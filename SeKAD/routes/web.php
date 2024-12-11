43b5236439b4552744674189fd4d9b071541c2ce
faizhebak <147373045+faizhebak@users.noreply.github.com>
2024-12-11 16:59:15 +0800

    1

---
+++ SeKAD/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\indexController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/',[indexController::class],'index');
