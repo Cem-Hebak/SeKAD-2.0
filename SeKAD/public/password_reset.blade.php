576235b608291c21e7948e7461c9cb1a21261e5e
Affif <affiffarhan2003@gmail.com>
2024-12-16 13:20:24 +0800

    acc recovery

--- SeKAD/public/password_reset.blade.php
+++ SeKAD/public/password_reset.blade.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - SeKAD</title>
+    <title>Forgot Password - SeKAD</title>
     <link rel="stylesheet" href="css/login.css">
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <div class="login-box">
         <a href="index" class="logo logo-admin">
             <img src="img/logo-sm-dark.png" height="50" alt="logo" class="auth-logo">
         </a>
         <h2>SeKAD</h2>
-        <p>Log In</p>
-        
-        <form action="password_reset_check.blade.php" method="POST">
+        <p>Forgot Password</p>
+
+        <form action="password/email" method="POST">
             <input type="email" name="email" placeholder="Email Address" required>
-            <button type="submit" class="btn-login">Log In</button>
+            <button type="submit" class="btn-login">Reset Password</button>
         </form>
 
-        <a href="password_reset.php">Forgot your password?</a>
+        <a href="login.blade.php">Log In</a>
     </div>
 </body>
+
 </html>
--- SeKAD/routes/web.php
+++ SeKAD/routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\indexController;
+use App\Http\Controllers\PasswordResetController;
+
+// Handle the password reset form submission
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/',[indexController::class],'index');
+Route::get('/', [indexController::class], 'index');
