58af2acdcc43077900f4e77da70b547c4e8d9b90
Alif Aiman <alfmnsr23@gmail.com>
2024-12-16 22:27:38 +0800

    attendanceController

---
+++ SeKAD/app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Attendance;
+
+class AttendanceController extends Controller
+{
+    public function getLowAttendance()
+    {
+        $month = now()->month; // Current month
+        $lowAttendanceStudents = Attendance::lowAttendance($month)->get();
+
+        // Return as JSON (for AJAX call)
+        return response()->json($lowAttendanceStudents);
+    }
+}
+
