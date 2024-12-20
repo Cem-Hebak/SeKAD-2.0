c7f5ec23ee26e5f5e259a7648b6186b67d8b0c0c
Affif <affiffarhan2003@gmail.com>
2024-12-21 01:00:14 +0800

    Update forgot_password_check.blade.php

--- SeKAD/public/forgot_password_check.blade.php
+++ SeKAD/public/forgot_password_check.blade.php
@@ -47,7 +47,7 @@ if (isset($_POST['forgot'])) {
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
             $mail->Username = 'sekadinfo@gmail.com';
-            $mail->Password = 'odvd bied vomf bwcy';
+            $mail->Password = 'anoi ijio mygb yydj';
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
             $mail->setFrom('sekadinfo@gmail.com', 'SeKAD Support');
