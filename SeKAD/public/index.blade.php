2f677f588de402d790e8fa78bbb91309b06b9494
faizhebak <147373045+faizhebak@users.noreply.github.com>
2025-01-13 09:44:42 +0800

    1

--- SeKAD/public/index.blade.php
+++ SeKAD/public/index.blade.php
@@ -326,7 +326,11 @@
         <div class="owl-carousel header-carousel position-relative">
             <?php foreach ($announcements as $announcement): ?>
                 <div class="owl-carousel-item position-relative">
-                    <img class="img-fluid" src=<?= htmlspecialchars($announcement['pic']) ?> alt="Announcement Image">
+                <img class="img-fluid" 
+                            src="<?= htmlspecialchars($announcement['pic']) ?>" 
+                            alt="Announcement Image" 
+                            onerror="this.onerror=null; this.src='Sekolah.png';" 
+                            style="width: 1366px; height: 768px; object-fit: cover; max-width: 100%; max-height: 100%; display: block;">
                     <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
                         <div class="container">
                             <div class="row justify-content-start">
@@ -563,7 +567,7 @@
                 <?php endif; ?>
                 <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
                     <div class="service-item text-center pt-3">
-                        <a href="settings.blade.php" target="_blank">
+                        <a href="setting.blade.php" target="_blank">
                         <div class="p-4">
                             <i class="fa fa-3x fa-cog text-primary mb-4"></i>
                             <h5 class="mb-3">Settings</h5>
@@ -740,81 +744,40 @@
 
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Quick Link</h4>
-                    <a class="btn btn-link" href="">About SeKAD</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Privacy Policy</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">FAQs & Help</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Contact</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Sekolah USO, Nazi Camp</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>SMKUso@gmail.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Gallery</h4>
-                    <div class="row g-2 pt-2">
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Newsletter</h4>
-                    <p>Receive an email if theres any News</p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">SeKAD</a>, All Right Reserved.
-
-
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="AssignStudent.php">Home</a>
-                            <a href="">Cookies</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
-                    </div>
+    <div class="container py-5">
+        <div class="row g-5 justify-content-center text-center">
+            <!-- Quick Links -->
+            
+            <!-- Contact Information -->
+            <div class="">
+                <h4 class="text-white mb-4">Contact</h4>
+                <p class="mb-2">
+                    <i class="fa fa-map-marker-alt me-3"></i>
+                    Sekolah Menengah Sains Labuan, <br>
+                    Jalan Sungai Pagar 87032, <br> Wilayah Persekutuan Labuan
+                </p>
+                <p class="mb-2">
+                    <i class="fa fa-phone-alt me-3"></i>
+                    (+60) 87 461525 (Office),<br>
+                      (+60) 87 462835 (Fax)
+                </p>
+                <div class="d-flex justify-content-center pt-2">
+                    <a class="btn btn-outline-light btn-social me-2" href="https://www.facebook.com/share/1Ar5pkmhEn/?mibextid=wwXIfr">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="copyright d-flex flex-column align-items-center">
+            <div class="text-center mb-3">
+                &copy; <a class="text-light border-bottom" href="index.blade.php">SeKAD</a>, All Rights Reserved.
+            </div>
+            
+        </div>
+    </div>
+</div>
     <!-- Footer End -->
 
 
