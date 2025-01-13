ab26c8afce77e81cbf939b2eff514ada4a8b24e8
ImanAmier <muhammadimanamier@graduate.utm.my>
2025-01-14 01:54:08 +0800

    update

--- SeKAD/public/attendanceRecord1.blade.php
+++ SeKAD/public/attendanceRecord1.blade.php
@@ -4,7 +4,7 @@ include('db_connection.php'); // Include database connection
 // Ensure the session variable for name is set
 $full_name = $_SESSION['name'] ?? 'User'; // Fallback to 'User' if the name is not set
 $first_name = explode(' ', $full_name)[0]; // Extract the first name
-$role = htmlspecialchars($_SESSION['role'], ENT_QUOTES, 'UTF-8');
+$role = htmlspecialchars($_SESSION['role'], ENT_QUOTES, 'UTF-8');   
     
 
     // Retrieve user data from the session
